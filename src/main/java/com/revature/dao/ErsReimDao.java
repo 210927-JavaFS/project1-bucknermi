186c7b685fa98c9a123872c3e8274c07c58c3eae
bucknermi <michaeliwbuckner@gmail.com>
2021-10-19 21:04:58 -0500

    Creating and implementing all methods for reimbursements

--- src/main/java/com/revature/dao/ErsReimDao.java
+++ src/main/java/com/revature/dao/ErsReimDao.java
@@ -3,16 +3,27 @@ package com.revature.dao;
 import java.util.List;
 
 import com.revature.dao.model.ErsReim;
+import com.revature.dao.model.ErsUser;
 
 public interface ErsReimDao {
 	
-	public void addReimbursement(ErsReim er);
+	public void addReim(ErsReim er);
+	
+	public void updateReim(ErsReim er);
+	
+	public void deleteReim(ErsReim er);
 	
 	public List<ErsReim> getAll();
 	
 	public List<ErsReim> getAllByFalse();
 	
 	public List<ErsReim> getAllByTrue();
+	
+	public List <ErsReim> getAllByUser(ErsUser eu);
+	
+	
+	
+	
 	
 	
 }
--- src/main/java/com/revature/dao/ErsReimDaoImpl.java
+++ src/main/java/com/revature/dao/ErsReimDaoImpl.java
@@ -6,49 +6,68 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 import com.revature.dao.model.ErsReim;
+import com.revature.dao.model.ErsUser;
 import com.revature.util.ConfigUtil;
 
 public class ErsReimDaoImpl implements ErsReimDao {
 
 	@Override
-	public void addReimbursement(ErsReim er) {
+	public void addReim(ErsReim er) {
 		Session session = ConfigUtil.getSession();
 		Transaction tx;
-		try {
-			tx = session.beginTransaction();
-			session.save(er);
-			tx.commit();
-		}
+		tx = session.beginTransaction();
+		session.save(er);
+		tx.commit();
 
-		finally {
-			
+	}
 
-		}
+	@Override
+	public void updateReim(ErsReim er) {
+		Session session = ConfigUtil.getSession();
+		Transaction tx;
+		tx = session.beginTransaction();
+		session.merge(er);
+		tx.commit();
+
+	}
+
+	@Override
+	public void deleteReim(ErsReim er) {
+		Session session = ConfigUtil.getSession();
+		Transaction tx;
+		tx = session.beginTransaction();
+		session.delete(er);
+		tx.commit();
 
 	}
 
 	@Override
 	public List<ErsReim> getAll() {
 		Session session = ConfigUtil.getSession();
-		List<ErsReim> reims= session.createQuery("FROM ErsReim").list();
+		List<ErsReim> reims = session.createQuery("FROM ErsReim").list();
 		return reims;
 	}
 
 	@Override
 	public List<ErsReim> getAllByFalse() {
 		Session session = ConfigUtil.getSession();
-		List<ErsReim> reims= session.createQuery("FROM ErsReim E WHERE resolved = FALSE").list();
+		List<ErsReim> reims = session.createQuery("FROM ErsReim E WHERE resolved = FALSE").list();
 		return reims;
 	}
 
 	@Override
 	public List<ErsReim> getAllByTrue() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	
-		
+		Session session = ConfigUtil.getSession();
+		List<ErsReim> reims = session.createQuery("FROM ErsReim E WHERE resolved = TRUE").list();
+		return reims;
 	}
 
+	@Override
+	public List<ErsReim> getAllByUser(ErsUser eu) {
+		Session session = ConfigUtil.getSession();
+		int id = eu.getUser_id();
+		List<ErsReim> reims = session.createQuery("From ErsReim E where user_id ="+id).list();
+		return reims;
+	}
 
+}

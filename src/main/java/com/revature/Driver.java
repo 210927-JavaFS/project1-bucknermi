c1b7dc2e24ae1f423cc7b6aeb2b000bfa0f78596
bucknermi <michaeliwbuckner@gmail.com>
2021-10-30 21:12:20 -0500

    Update Driver.java

--- src/main/java/com/revature/Driver.java
+++ src/main/java/com/revature/Driver.java
@@ -16,7 +16,7 @@ public class Driver {
 			config.addStaticFiles("/Static", Location.CLASSPATH);
 		});
 		configure(new ReimController(), new ErsUserController());
-		app.start();
+		app.start("18.223.29.20",7000);
 	}
 	
 	public static void configure(Controller...controllers) {

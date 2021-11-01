96441489e22bf6b4e38fd54dfbad75cdb86968ac
bucknermi <michaeliwbuckner@gmail.com>
2021-11-01 16:19:21 -0500

    Update Driver.java

--- src/main/java/com/revature/Driver.java
+++ src/main/java/com/revature/Driver.java
@@ -17,7 +17,7 @@ public class Driver {
 		});
 		app.before(ctx->ctx.header("Access-Control-Allow-Origin", "http://project1-bucknermi.s3-website.us-east-2.amazonaws.com"));
 		app.before(ctx -> ctx.header("Access-Control-Allow-Credentials", "true"));
-		app.before(ctx -> ctx.header(" Access-Control-Allow-Headers", "*"));
+		app.before(ctx -> ctx.header(" Access-Control-Allow-Headers", "true"));
 		configure(new ReimController(), new ErsUserController());
 		app.start();
 	}
